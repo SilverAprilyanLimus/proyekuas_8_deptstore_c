5a5c42931ca761824d7a975381b482ce173b1cfd
RobertoJessons <soson3435@gmail.com>
2024-06-04 16:17:03 +0700

    utils

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -23,13 +23,13 @@ export const formatDateToLocal = (
 
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
-  // based on highest record and in 1000s
+  // based on the highest record and in 1000s
   const yAxisLabels = [];
   const highestRecord = Math.max(...revenue.map((month) => month.revenue));
   const topLabel = Math.ceil(highestRecord / 1000) * 1000;
 
-  for (let i = topLabel; i >= 0; i -= 1000) {
-    yAxisLabels.push(`$${i / 1000}K`);
+  for (let i = topLabel; i >= 0; i -= 100000) {
+    yAxisLabels.push(`Rp${i / 1000}K`); // i divided by 1000 and then multiplied back to format correctly
   }
 
   return { yAxisLabels, topLabel };
